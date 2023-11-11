6f4d8318d90638f6644245727b9cca76aabf031e
MaxKorovchenko <max.korovchenko@gmail.com>
2023-11-11 18:20:03 +0200

    add home page content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,28 @@
-const styles = {
-  title: {
-    fontSize: 50,
-    color: 'aliceblue',
-    textAlign: 'center',
-    marginTop: 200,
-  },
-};
+import phonebook from 'assets/images/phonebook.jpg';
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 style={styles.title}>Welcome to the Phonebook APP</h1>
+    <div className={styles.container}>
+      <h1>Welcome to Your Phonebook!</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.textWrapper}>
+          <p className={styles.text}>
+            Discover the joy of managing your contacts with our user-friendly
+            phonebook app.
+          </p>
+          <p className={styles.text}>
+            Register in a few simple steps and unlock a world of organized
+            communication.
+          </p>
+          <p className={styles.text}>
+            Effortlessly add, edit, and search for contacts with our intuitive
+            interface.
+          </p>
+        </div>
+
+        <img className={styles.image} src={phonebook} alt="phonebook" />
+      </div>
     </div>
   );
 };
