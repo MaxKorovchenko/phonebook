2c7da17353aa50e25e027258bdd19626fd986c69
MaxKorovchenko <109962117+MaxKorovchenko@users.noreply.github.com>
2023-11-14 13:35:40 +0000

    Deploying to gh-pages from @ MaxKorovchenko/phonebook@c8ec9b048ef5abbea30cf2542e8ea774c0711ad7 🚀

---
+++ static/js/520.e165e614.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[520],{4520:function(e,n,t){t.r(n);var l=t(3329),o={number:{fontSize:120,color:"aliceblue",textAlign:"center",marginTop:100},title:{fontSize:45,color:"aliceblue",textAlign:"center"}};n.default=function(){return(0,l.jsxs)("div",{children:[(0,l.jsx)("p",{style:o.number,children:"404"}),(0,l.jsx)("h2",{style:o.title,children:"Page not found"})]})}}}]);
+//# sourceMappingURL=520.e165e614.chunk.js.map
